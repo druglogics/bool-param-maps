7f914423fd7f9f4abaf40c53771e6d61a4e6f59e
john <bblodfon@gmail.com>
2020-09-04 12:09:37 +0200

    add script thats generate the link operator matrix data
    Former-commit-id: 13c2886a798f1612ad31d6969fd41e4607381789
    Former-commit-id: 8152583d0182ba1be9c8917e1c08227c5bd13f47

---
+++ scripts/get_lo_mat.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(stringr)
+library(usefun)
+
+models_ss_stats = readRDS(file = "data/models_ss_stats.rds")
+node_stats = readRDS(file = "data/node_stats.rds")
+lo_nodes = node_stats %>% pull(node)
+num_bits = length(lo_nodes)
+
+# make data wider by transforming the model name from decimal
+# to it's binary link operator representation
+lo_mat = sapply(models_ss_stats %>% pull(model_number), function(model_name) {
+  stringr::str_split(
+    string = usefun::dec_to_bin(decimal_num = model_name, bits = num_bits),
+    simplify = TRUE, # return matrix
+    pattern = "")} %>%
+    as.integer()) %>%
+  t() # transpose result => columns are now the nodes
+
+# name the columns as the link-operator nodes
+colnames(lo_mat) = lo_nodes
+
+# save data
+# `lo_mat` has 23 columns for each node, describing all possible
+# link-operator parameterizations (from 0...0 to 1...0). As such,
+# it has 2^23 rows.
+saveRDS(object = lo_mat, file = "data/lo_mat.rds")

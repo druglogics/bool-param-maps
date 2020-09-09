2bf4f1f62b336667359249f75b63c9edb436deb7
john <bblodfon@gmail.com>
2020-09-09 19:43:40 +0200

    add code for UMAP vs model numeric representation figures
    Former-commit-id: aa7ceef5bf1d2b5f99cf552616f68bc3c6cbbaeb
    Former-commit-id: 41a8c60c51a94754eabd2953c9c80d9900910df3
    Former-commit-id: 266a109072f8fc72bbdffff6ef5413d3ce8ce701
    Former-commit-id: 9a6ba48ff9f84310f2422a188be1d9480428fae9

--- scripts/param_ss_umap_vis.R
+++ scripts/param_ss_umap_vis.R
@@ -8,6 +8,7 @@ library(ggplot2)
 # get number of ss per model
 models_ss_stats = readRDS(file = "data/models_ss_stats.rds")
 ss_num = models_ss_stats %>% pull(ss_num) %>% as.factor()
+model_number = models_ss_stats %>% pull(model_number)
 
 # visualize unsupervised dataset
 lo_umap = readRDS(file = "data/lo_umap.rds")
@@ -16,13 +17,20 @@ model_samples_indexes = readRDS(file = "data/indexes.rds")
 colnames(lo_umap) = c('X','Y')
 data = lo_umap %>%
   as_tibble() %>%
-  mutate(ss_num = ss_num[model_samples_indexes])
+  mutate(ss_num = ss_num[model_samples_indexes]) %>%
+  mutate(model_num = model_number[model_samples_indexes])
 
 ggplot(data) +
   geom_point(aes(x = X, y = Y, color = ss_num), size = 0.01) +
   theme_classic() +
   guides(colour = guide_legend(title = "#Fixpoints", override.aes = list(size = 12)))
-ggsave(filename = "img/umap_unsupervised_s001.png", dpi = "print", width = 7, height = 5)
+ggsave(filename = "img/umap_unsupervised.png", dpi = "print", width = 7, height = 5)
+
+ggplot(data) +
+  geom_point(aes(x = X, y = Y, color = model_num), size = 0.01) +
+  theme_classic() +
+  scale_color_distiller(palette = "Spectral", guide = guide_colourbar(title = "Model Number"))
+ggsave(filename = "img/umap_unsupervised_model_num.png", dpi = "print", width = 7, height = 5)
 
 # visualize supervised dataset
 lo_sumap = readRDS(file = "data/lo_sumap.rds")
@@ -31,10 +39,17 @@ model_samples_indexes_sup = readRDS(file = "data/indexes_sup.rds")
 colnames(lo_sumap) = c('X','Y')
 data = lo_sumap %>%
   as_tibble() %>%
-  mutate(ss_num = ss_num[model_samples_indexes_sup])
+  mutate(ss_num = ss_num[model_samples_indexes_sup]) %>%
+  mutate(model_num = model_number[model_samples_indexes_sup])
+
+ggplot(data) +
+  geom_point(aes(x = X, y = Y, color = model_num), size = 0.01) +
+  theme_classic() +
+  scale_color_distiller(palette = "Spectral", guide = guide_colourbar(title = "Model Number"))
+ggsave(filename = "img/umap_supervised_model_num.png", dpi = "print", width = 7, height = 5)
 
 ggplot(data) +
   geom_point(aes(x = X, y = Y, color = ss_num), size = 0.01) +
   theme_classic() +
   guides(colour = guide_legend(title = "#Fixpoints", override.aes = list(size = 12)))
-ggsave(filename = "img/umap_supervised_s001.png", dpi = "print", width = 7, height = 5)
+ggsave(filename = "img/umap_supervised.png", dpi = "print", width = 7, height = 5)

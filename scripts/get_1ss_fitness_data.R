cb294e132e1b112aef656f531de947caa4069c2a
john <bblodfon@gmail.com>
2020-09-14 13:39:48 +0200

    add script and result data (model fitness)
    Models have only 1 stable state and the fitness to the AGS steady
    state is calculated and saved. Also the AGS steady state itself is
    saved for loading speed.
    
    
    Former-commit-id: 606fcfc87529e5f7617075388eb670ad0acb6b7e
    Former-commit-id: 9374a1c24c5f9147934a7ec3b7f9f31af1e2c2a8
    Former-commit-id: d122898efa862a732da6709826a1bf0fcede5e75
    Former-commit-id: 0074422e3bae23657539f3bf65c2d243d24b08db

---
+++ scripts/get_1ss_fitness_data.R
@@ -0,0 +1,36 @@
+###############################################
+# For all the models that have 1 stable state,
+# get their fitness to the AGS steady state
+###############################################
+
+library(dplyr)
+library(tibble)
+library(stringr)
+library(usefun)
+
+# Read the models stable state data (see `get_ss_data.R` script)
+ss_data = readRDS(file = "data/ss_data.rds")
+
+# get the AGS steady state
+steady_state_file = "data/steadystate"
+lines = readLines(steady_state_file)
+ss_str = unlist(strsplit(x = lines[8], split = "\t"))
+ss_mat = stringr::str_split(string = ss_str, pattern = ":", simplify = TRUE)
+colnames(ss_mat) = c("nodes", "states")
+ss_tbl = ss_mat %>% as_tibble() %>% mutate_at(vars(states), as.integer)
+
+steady_state = ss_tbl %>% pull(states)
+names(steady_state) = ss_tbl %>% pull(nodes)
+
+# save `steady_state` vector
+saveRDS(object = steady_state, file = "data/ags_steady_state.rds")
+
+# calculate models fitness to AGS steady state
+fit_data = apply(ss_data[, names(steady_state)], 1,
+  usefun::get_percentage_of_matches, steady_state)
+
+# data check
+stopifnot(all(names(fit_data) == rownames(ss_data)))
+
+# save result
+saveRDS(object = fit_data, file = "data/fit_data.rds")

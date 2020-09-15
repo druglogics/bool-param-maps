ec360ca48879144a123bd341887a17b0753d9da7
john <bblodfon@gmail.com>
2020-09-15 16:35:19 +0200

    add script (UMAP on hte parameterization data of the 1ss models)
    Former-commit-id: 992e6994cf3c344a691f695111c5fe87321cd97c
    Former-commit-id: ed885a53d8fc88c49cdbdf580e98a3de1bfabb51
    Former-commit-id: 53bba1820f7fd005b0e8b5cbeb6a4ccf92f91cc4
    Former-commit-id: 689195da2890bf2da042205947c92ed8114e2d1e

---
+++ scripts/1ss_models_umap.R
@@ -0,0 +1,29 @@
+########################
+# UMAP for 1 ss models #
+########################
+
+library(dplyr)
+library(tibble)
+library(ggplot2)
+library(uwot)
+
+lo_data = readRDS(file = "lo_data.rds")
+
+for(n_neighbors in 2:20) {
+  print(n_neighbors)
+  set.seed(42)
+
+  lo_umap = uwot::umap(X = lo_data, metric = "manhattan", n_threads = 8, n_neighbors = n_neighbors)
+
+  # I have saved them all but they take a lot of space, so will just keep
+  # a small number of them in the repo (and to use/color in further analyses)
+  saveRDS(object = lo_umap, file = paste0("1ss_lo_umap/lo_umap_1ss_", n_neighbors, "nn.rds"))
+
+  lo_umap %>%
+    `colnames<-` (c("X", "Y")) %>%
+    as_tibble() %>%
+    ggplot(aes(x = X, y = Y)) + geom_point(size = 0.01) + theme_classic()
+
+  # save the images
+  ggsave(filename = paste0("img/1ss_umap/1ss_umap_unsup_", n_neighbors, ".png"), dpi = "print", width = 7, height = 5)
+}

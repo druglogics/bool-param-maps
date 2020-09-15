737aafe392e72986adb8615f1c1b315f1cc52c9d
john <bblodfon@gmail.com>
2020-09-15 16:09:40 +0200

    add emba analysis script
    Former-commit-id: 6703f8e6e0064fdeb83029ce62a74d381aaa4602
    Former-commit-id: 2d204a625aa535886ebc074d55053695eaa14bb2
    Former-commit-id: 24e67e0ade3b1ae6e414b4eee3412f244f2f9f5c
    Former-commit-id: 5fa78fa540200ebbb5eb4f9ffeafe5a7abbb23b8

---
+++ scripts/emba_analysis.R
@@ -0,0 +1,49 @@
+# In this analysis the data used includes only the models with 1 stable state
+library(dplyr)
+library(emba)
+
+# Read the models stable state data (see `get_ss_data.R` script)
+ss_data = readRDS(file = "data/ss_data.rds")
+
+# Read the model predictions (see `get_model_predictions.R` script)
+pred_data = readRDS(file = "data/model_predictions.rds")
+
+# check the model names have the same order
+stopifnot(all(rownames(ss_data) == rownames(pred_data)))
+
+# Read the link operator data (see `get_1ss_lo_data.R`)
+lo_data = readRDS(file = "data/lo_data.rds")
+
+# check the model names have the same order
+stopifnot(all(rownames(pred_data) == rownames(lo_data)))
+
+# observed synergies
+obs_syn = emba::get_observed_synergies(file = "data/observed_synergies_cascade_1.0")
+
+# Performance Biomarker Analysis
+# split models to a different number of MCC classes
+# 14 was the total number of unique MCC scores
+for (mcc_classes in 3:14) {
+  print(mcc_classes)
+  mcc_res = emba::biomarker_mcc_analysis(
+    model.predictions = pred_data,
+    models.stable.state = ss_data,
+    models.link.operator = lo_data,
+    observed.synergies = obs_syn,
+    threshold = 0.6,
+    num.of.mcc.classes = mcc_classes,
+    penalty = 0.1)
+  # in the repo we are going to keep only a few of them to save space :)
+  saveRDS(object = mcc_res, file = paste0("data/mcc_", mcc_classes, "_res.rds"))
+}
+
+# Synergy Biomarker analysis
+synergy_res = emba::biomarker_synergy_analysis(
+  model.predictions = pred_data,
+  models.stable.state = ss_data,
+  models.link.operator = lo_data,
+  observed.synergies = obs_syn,
+  threshold = 0.6,
+  calculate.subsets.stats = FALSE,
+  penalty = 0.1)
+saveRDS(object = synergy_res, file = "data/synergy_res.rds")

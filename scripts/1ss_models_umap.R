6e44c1dade3c03ea2d0cebe0056f49dec0c8e88e
john <bblodfon@gmail.com>
2020-09-21 12:46:58 +0200

    add title and pixel-size points
    Former-commit-id: feea54e035645d576a0ba831e2a9a8a71e50a534
    Former-commit-id: 6758ecddad90b4dce658c74ef78fb374b2d48c77
    Former-commit-id: da1945a1312fb8b0e12b9656171899597b92979b
    Former-commit-id: 6557576c1bd8dd8dcdb3bfb6fe2041128bda5ddd

--- scripts/1ss_models_umap.R
+++ scripts/1ss_models_umap.R
@@ -14,6 +14,8 @@ for(n_neighbors in 2:20) {
   set.seed(42)
 
   lo_umap = uwot::umap(X = lo_data, metric = "manhattan", n_threads = 8, n_neighbors = n_neighbors)
+  # in case you don't want to re-run the above command and have all the .rds result files
+  #lo_umap = readRDS(file = paste0("data/1ss_lo_umap/lo_umap_1ss_", n_neighbors, "nn.rds"))
 
   # I have saved them all but they take a lot of space, so I will just keep
   # a small number of them in the repo, namely for n_neighbors = 2,4,6,8,11 and 14
@@ -22,7 +24,11 @@ for(n_neighbors in 2:20) {
   lo_umap %>%
     `colnames<-` (c("X", "Y")) %>%
     as_tibble() %>%
-    ggplot(aes(x = X, y = Y)) + geom_point(size = 0.01) + theme_classic()
+    ggplot(aes(x = X, y = Y)) +
+      geom_point(shape = '.') +
+      labs(title = paste0("Parameterization Map - ", n_neighbors, " Neighbours")) +
+      theme_classic() +
+      theme(plot.title = element_text(hjust = 0.5))
 
   # save the images
   ggsave(filename = paste0("img/1ss_umap/1ss_umap_unsup_", n_neighbors, ".png"), dpi = "print", width = 7, height = 5)

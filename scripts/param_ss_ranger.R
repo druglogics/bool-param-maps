d2a0c0e77822cc22843d5a30f8540c52faef77cb
john <bblodfon@gmail.com>
2020-09-24 14:38:06 +0200

    remove comment
    Former-commit-id: 62fe8e6ff89a6b4e002438126ca4f5e0523c8929
    Former-commit-id: 1323864d62c681d4fa4ed90cabfee139b74c75f6
    Former-commit-id: d7d671ac8a59f2146aea9cc061162796b4256041
    Former-commit-id: f38070d7770200348fe6408df9eaaba474fd4145

--- scripts/param_ss_ranger.R
+++ scripts/param_ss_ranger.R
@@ -17,7 +17,7 @@ set.seed(42)
 model_samples_indexes = sample(x = 1:nrow(lo_mat), size = 4000000)
 ranger_res = ranger::ranger(x = lo_mat[model_samples_indexes,],
   y = ss_num[model_samples_indexes],
-  num.trees = 300, mtry = 16, #save.memory = TRUE,
+  num.trees = 300, mtry = 16,
   num.threads = 4, importance = 'impurity')
 
 saveRDS(object = ranger_res, file = "data/ranger_res.rds")

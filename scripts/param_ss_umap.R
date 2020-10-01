627774c8259ce41c02801e5ab317852dc3298700
john <bblodfon@gmail.com>
2020-10-01 18:26:56 +0200

    update script (use all the dataset)
    I overcame memory problems by adding more swap space to the server :)
    
    
    Former-commit-id: 0946b16e0c1154967878b5936f342a1b06662a5a
    Former-commit-id: fc79cc78a971d19730ce31f9b261a5691ebd27b5
    Former-commit-id: bfe7cac8033e0d9fb67a63c527cd3024dd4da15d
    Former-commit-id: a0383125049c02b8ff5e0a4e9e2439efbd875c4f

--- scripts/param_ss_umap.R
+++ scripts/param_ss_umap.R
@@ -1,41 +1,44 @@
-########
-# UMAP #
-########
-
+################################################################
+# Use UMAP on the whole CASCADE 1.0 dataset (parameterization) #
+################################################################
 library(dplyr)
 library(tibble)
-# Use `uwot` library (fast, multiple thread support)
 library(uwot)
 
 # NOTE: we suggest having a lot of memory to run this script! (64GB should be enough)
-# Memory problems was the reason we do not run UMAP on the whole dataset
 
 # read data
-models_ss_stats = readRDS(file = "data/models_ss_stats.rds")
-lo_mat = readRDS(file = "data/lo_mat.rds")
+models_ss_stats = readRDS(file = "data/models_ss_stats.rds") # see `count_models_ss.R`
+lo_mat = readRDS(file = "data/lo_mat.rds") # see `get_lo_mat.R`
+# order of models is alright, since `lo_mat` was produced from `models_ss_stats`
+
+################
+# Unsupervised #
+################
+
+# set neighbors
+n_neighbors = c(2,8,14,20) # local to global
+
+for (i in n_neighbors) {
+  print(paste0("#Neighbors: ", i))
+
+  #indx = sample(x = 1:nrow(lo_mat), size = 50000)
+  set.seed(42)
+  lo_umap = uwot::umap(X = lo_mat, n_threads = 8, n_neighbors = i, verbose = TRUE)
+  saveRDS(object = lo_umap, file = paste0("data/lo_umap/lo_umap_", i, "nn.rds"))
+}
+
+##############
+# Supervised #
+##############
 
 # classification: `ss_num` as factor
 ss_num = models_ss_stats %>% pull(ss_num) %>% as.factor()
 
-# choose seed and model sample for unsupervised case
-set.seed(42)
-n_samples = 6000000
-model_samples_indexes = sample(x = 1:nrow(lo_mat), size = n_samples)
-saveRDS(object = model_samples_indexes, file = "data/indexes.rds")
+for (i in n_neighbors) {
+  print(paste0("#Neighbors: ", i))
 
-lo_umap = uwot::umap(
-  X = lo_mat[model_samples_indexes,],
-  metric = "manhattan", n_threads = 4) # put more `n_threads` if you have them!
-saveRDS(object = lo_umap, file = "data/lo_umap.rds")
-
-# choose seed and model sample for the supervised case
-set.seed(42)
-n_samples_sup = nrow(lo_mat)/3 # 1/3 of the dataset, ~ 3 million rows
-model_samples_indexes_sup = sample(x = 1:nrow(lo_mat), size = n_samples_sup)
-saveRDS(object = model_samples_indexes_sup, file = "data/indexes_sup.rds")
-
-lo_sumap = uwot::umap(
-  X = lo_mat[model_samples_indexes_sup,],
-  y = ss_num[model_samples_indexes_sup],
-  metric = "manhattan", n_threads = 4) # put more `n_threads` if you have them!
-saveRDS(object = lo_sumap, file = "data/lo_sumap.rds")
+  set.seed(42)
+  lo_sumap = uwot::umap(X = lo_mat, y = ss_num, n_threads = 8, n_neighbors = i, verbose = TRUE)
+  saveRDS(object = lo_sumap, file = paste0("data/lo_umap/lo_sumap_", i, "nn.rds"))
+}

201e3e36a5b647d86395fa8826ed809d6da33ba9
john <bblodfon@gmail.com>
2020-10-04 00:19:23 +0200

    update script (create figures for newest data results, improve figure)
    Former-commit-id: 4d5bbfb1f5f7845ad97a5ef6dc1fffb7da28c542
    Former-commit-id: 8014b8ed538960100a58e317270bf4f1049ba26c
    Former-commit-id: 7948dc6120f208411b5290f23a19e316ce0bcfe7
    Former-commit-id: e7a99f5c8334c992e84eff6634d6a7fe07a4fb39

--- scripts/param_ss_umap_vis.R
+++ scripts/param_ss_umap_vis.R
@@ -1,55 +1,134 @@
-##########################
-# UMAP visualize results #
-##########################
-
+###############################################################
+# Visualize parameterization of the whole CASCADE 1.0 dataset #
+# Data results are from the `param_ss_umap.R` script          #
+###############################################################
 library(dplyr)
 library(ggplot2)
+library(scales)
 
 # get number of ss per model
-models_ss_stats = readRDS(file = "data/models_ss_stats.rds")
+models_ss_stats = readRDS(file = "data/models_ss_stats.rds") # see `count_models_ss.R`
 ss_num = models_ss_stats %>% pull(ss_num) %>% as.factor()
 model_number = models_ss_stats %>% pull(model_number)
 
-# visualize unsupervised dataset
-lo_umap = readRDS(file = "data/lo_umap.rds")
-model_samples_indexes = readRDS(file = "data/indexes.rds")
+################
+# Unsupervised #
+################
 
-colnames(lo_umap) = c('X','Y')
+n_neighbors = c(8,14,20) # local to global
+
+# visualize the results with `euclidean` distance metric
+for (i in n_neighbors) {
+  print(paste0("#Neighbors: ", i))
+
+  # get umap coordinates
+  lo_umap = readRDS(file = paste0("data/lo_umap/lo_umap_", i, "nn.rds"))
+
+  # tidy data
+  data = lo_umap %>%
+    `colnames<-` (c("X", "Y")) %>%
+    tibble::as_tibble() %>%
+    tibble::add_column(ss_num = ss_num, model_num = model_number)
+
+  # color by #fixpoints
+  data %>%
+    ggplot(aes(x = X, y = Y, color = ss_num)) +
+    geom_point(shape = '.') +
+    guides(colour = guide_legend(title = "#Fixpoints", label.theme = element_text(size = 12),
+      override.aes = list(shape = 19, size = 12))) +
+    labs(title = paste0("Parameterization Map (All CASCADE 1.0 models) - ", i, " Neighbors")) +
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5))
+  ggsave(filename = paste0("img/all_models_maps/umap_", i, "nn.png"), dpi = "print", width = 7, height = 5)
+
+  # color by model decimal representation
+  data %>%
+    ggplot(aes(x = X, y = Y, color = model_num)) +
+    geom_point(shape = '.') +
+    scale_color_distiller(palette = "Spectral", labels = scales::label_number_si(),
+      guide = guide_colourbar(title = "Model Number")) +
+    labs(title = paste0("Parameterization Map (All CASCADE 1.0 models) - ", i, " Neighbors")) +
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5))
+  ggsave(filename = paste0("img/all_models_maps/umap_", i , "nn_model_num.png"), dpi = "print", width = 7, height = 5)
+}
+
+# visualize the results with `hamming` distance metric
+lo_umap = readRDS(file = "data/lo_umap/lo_umap_14nn_ham_0_5_min_dist.rds")
+
+# tidy data
 data = lo_umap %>%
-  as_tibble() %>%
-  mutate(ss_num = ss_num[model_samples_indexes]) %>%
-  mutate(model_num = model_number[model_samples_indexes])
+  `colnames<-` (c("X", "Y")) %>%
+  tibble::as_tibble() %>%
+  tibble::add_column(ss_num = ss_num, model_num = model_number)
 
-ggplot(data) +
-  geom_point(aes(x = X, y = Y, color = ss_num), size = 0.01) +
+# color by #fixpoints
+data %>%
+  ggplot(aes(x = X, y = Y, color = ss_num)) +
+  geom_point(shape = '.') +
+  guides(colour = guide_legend(title = "#Fixpoints", label.theme = element_text(size = 12),
+    override.aes = list(shape = 19, size = 12))) +
+  labs(title = paste0("Parameterization Map (All CASCADE 1.0 models) - ", i, " Neighbors")) +
   theme_classic() +
-  guides(colour = guide_legend(title = "#Fixpoints", override.aes = list(size = 12)))
-ggsave(filename = "img/umap_unsupervised.png", dpi = "print", width = 7, height = 5)
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave(filename = paste0("img/all_models_maps/ham_umap_", i, "nn.png"), dpi = "print", width = 7, height = 5)
 
-ggplot(data) +
-  geom_point(aes(x = X, y = Y, color = model_num), size = 0.01) +
+# color by model decimal representation
+data %>%
+  ggplot(aes(x = X, y = Y, color = model_num)) +
+  geom_point(shape = '.') +
+  scale_color_distiller(palette = "Spectral", labels = scales::label_number_si(),
+    guide = guide_colourbar(title = "Model Number")) +
+  labs(title = paste0("Parameterization Map (All CASCADE 1.0 models) - ", i, " Neighbors")) +
   theme_classic() +
-  scale_color_distiller(palette = "Spectral", guide = guide_colourbar(title = "Model Number"))
-ggsave(filename = "img/umap_unsupervised_model_num.png", dpi = "print", width = 7, height = 5)
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave(filename = paste0("img/all_models_maps/ham_umap_", i , "nn_model_num.png"), dpi = "print", width = 7, height = 5)
 
-# visualize supervised dataset
-lo_sumap = readRDS(file = "data/lo_sumap.rds")
-model_samples_indexes_sup = readRDS(file = "data/indexes_sup.rds")
+##############
+# Supervised #
+##############
 
-colnames(lo_sumap) = c('X','Y')
+# visualize the results with `euclidean` distance metric
+for (i in n_neighbors) {
+  print(paste0("#Neighbors: ", i))
+
+  # get umap coordinates
+  lo_sumap = readRDS(file = paste0("data/lo_umap/lo_sumap_", i, "nn.rds"))
+
+  # tidy data
+  data = lo_sumap %>%
+    `colnames<-` (c("X", "Y")) %>%
+    tibble::as_tibble() %>%
+    tibble::add_column(ss_num = ss_num)
+
+  # color by #fixpoints
+  data %>%
+    ggplot(aes(x = X, y = Y, color = ss_num)) +
+    geom_point(shape = '.') +
+    guides(colour = guide_legend(title = "#Fixpoints", label.theme = element_text(size = 12),
+      override.aes = list(shape = 19, size = 12))) +
+    labs(title = paste0("Parameterization Supervised Map (All CASCADE 1.0 models) - ", i, " Neighbors")) +
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5))
+  ggsave(filename = paste0("img/all_models_maps/sumap_", i, "nn.png"), dpi = "print", width = 7, height = 5)
+}
+
+# visualize the results with `euclidean` distance metric, 14 `n_neighbors` and `min_dist` = 0.3
+lo_sumap = readRDS(file = "data/lo_umap/lo_sumap_14nn_0_3_min_dist.rds")
+
+# tidy data
 data = lo_sumap %>%
-  as_tibble() %>%
-  mutate(ss_num = ss_num[model_samples_indexes_sup]) %>%
-  mutate(model_num = model_number[model_samples_indexes_sup])
+  `colnames<-` (c("X", "Y")) %>%
+  tibble::as_tibble() %>%
+  tibble::add_column(ss_num = ss_num)
 
-ggplot(data) +
-  geom_point(aes(x = X, y = Y, color = model_num), size = 0.01) +
+# color by #fixpoints
+data %>%
+  ggplot(aes(x = X, y = Y, color = ss_num)) +
+  geom_point(shape = '.') +
+  guides(colour = guide_legend(title = "#Fixpoints", label.theme = element_text(size = 12),
+    override.aes = list(shape = 19, size = 12))) +
+  labs(title = paste0("Parameterization Supervised Map (All CASCADE 1.0 models)")) +
   theme_classic() +
-  scale_color_distiller(palette = "Spectral", guide = guide_colourbar(title = "Model Number"))
-ggsave(filename = "img/umap_supervised_model_num.png", dpi = "print", width = 7, height = 5)
-
-ggplot(data) +
-  geom_point(aes(x = X, y = Y, color = ss_num), size = 0.01) +
-  theme_classic() +
-  guides(colour = guide_legend(title = "#Fixpoints", override.aes = list(size = 12)))
-ggsave(filename = "img/umap_supervised.png", dpi = "print", width = 7, height = 5)
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave(filename = paste0("img/all_models_maps/sumap_14nn_0_3_min_dist.png"), dpi = "print", width = 7, height = 5)

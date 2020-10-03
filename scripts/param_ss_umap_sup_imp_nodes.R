a7aab248974106e13b55b2c48a07eceb8d980dab
john <bblodfon@gmail.com>
2020-10-04 00:41:04 +0200

    update script (whole dataset results are used now)
    Former-commit-id: 67511ebdd66dae1c641a9287a4296e2a95a69783
    Former-commit-id: 1353f9d7f3399dfbc9c8fc8092faeba775b1340b
    Former-commit-id: b6d0a0b6da95515979dfc46cfa915d3c57fc0bd9
    Former-commit-id: 7d84ad30f95cce5a0fd5121ff0230cc6e4e02bb9

--- scripts/param_ss_umap_sup_imp_nodes.R
+++ scripts/param_ss_umap_sup_imp_nodes.R
@@ -1,129 +1,85 @@
-##########################
-# UMAP visualize results #
-##########################
-
+# Embed the link-operator values of important nodes on the
+# parameterization maps generated with unsupervised and supervised
+# umap (using all the models of the CASCADE 1.0 dataset)
 library(dplyr)
 library(tibble)
-library(usefun)
 library(ggplot2)
 
-# get number of ss per model
-models_ss_stats = readRDS(file = "data/models_ss_stats.rds")
-ss_num = models_ss_stats %>% pull(ss_num) %>% as.factor()
-model_number = models_ss_stats %>% pull(model_number)
+# get umap coordinates (see `param_ss_umap.R`)
+lo_umap = readRDS(file = "data/lo_umap/lo_umap_20nn.rds")
+lo_umap_ham = readRDS(file = "data/lo_umap/lo_umap_14nn_ham_0_5_min_dist.rds")
+lo_sumap = readRDS(file = "data/lo_umap/lo_sumap_14nn_0_3_min_dist.rds")
 
-# get important node indexes
-lo_mat = readRDS(file = "data/lo_mat.rds")
-mapk14_index = which(colnames(lo_mat) == "MAPK14")
-erkf_index   = which(colnames(lo_mat) == "ERK_f")
-mekf_index   = which(colnames(lo_mat) == "MEK_f")
-pten_index   = which(colnames(lo_mat) == "PTEN")
-mtorc1_index = which(colnames(lo_mat) == "mTORC1_c")
-cflar_index  = which(colnames(lo_mat) == "CFLAR")
-n_bits = ncol(lo_mat) # n_bits = 23
+# get link-operator data
+lo_mat = readRDS(file = "data/lo_mat.rds") # see `get_lo_mat.R`
 
-# UMAP supervised results (see `param_ss_umap.R`)
-# 3 main clusters corresponding to the 0, 1 and 2 fixpoint models
-lo_sumap = readRDS(file = "data/lo_sumap.rds")
-colnames(lo_sumap) = c('X','Y')
-# 33% of the models used
-model_samples_indexes_sup = readRDS(file = "data/indexes_sup.rds")
+# The important nodes (2 last are the least important)
+imp_nodes = c("MAPK14", "ERK_f", "MEK_f", "PTEN", "mTORC1_c", "CFLAR", "CYCS")
 
-# get the model numbers in decimal format
-model_num = model_number[model_samples_indexes_sup]
+################
+# Unsupervised #
+################
 
-print("Get MAPK14 link operators")
-mapk14_param = sapply(model_num, function(num) {
-  bin_n = usefun::dec_to_bin(decimal_num = num, bits = n_bits)
-  substr(bin_n, mapk14_index, mapk14_index) %>% as.integer()
-})
+# hamming metric, 14 neighbors, min_dist = 0.5
+for (node in imp_nodes) {
+  print(paste0("Node: ", node))
 
-print("Get ERK_f link operators")
-erkf_param = sapply(model_num, function(num) {
-  bin_n = usefun::dec_to_bin(decimal_num = num, bits = n_bits)
-  substr(bin_n, erkf_index, erkf_index) %>% as.integer()
-})
+  lo_umap_ham %>%
+    `colnames<-` (c("X", "Y")) %>%
+    tibble::as_tibble() %>%
+    tibble::add_column(node_lo = lo_mat[,node] %>% as.factor()) %>%
+    ggplot(aes(x = X, y = Y, colour = node_lo)) +
+    geom_point(shape = '.') +
+    scale_colour_brewer(palette = "Set1", labels = c("AND-NOT", "OR-NOT")) +
+    guides(colour = guide_legend(title = paste0(node, " LO"), label.theme = element_text(size = 12),
+      override.aes = list(shape = 19, size = 12))) +
+    labs(title = paste0("Parameterization Unsupervised Hamming Map (14 Neighbors)")) +
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5))
 
-print("Get MEK_f link operators")
-mekf_param = sapply(model_num, function(num) {
-  bin_n = usefun::dec_to_bin(decimal_num = num, bits = n_bits)
-  substr(bin_n, mekf_index, mekf_index) %>% as.integer()
-})
+  ggsave(filename = paste0("img/imp_nodes_param_ss_maps/unsup_", node, "_ham.png"), dpi = "print", width = 7, height = 5)
+}
 
-print("Get PTEN link operators")
-pten_param = sapply(model_num, function(num) {
-  bin_n = usefun::dec_to_bin(decimal_num = num, bits = n_bits)
-  substr(bin_n, pten_index, pten_index) %>% as.integer()
-})
+# euclidean metric, 20 neighbors
+for (node in imp_nodes) {
+  print(paste0("Node: ", node))
 
-print("Get mTORC1_c link operators")
-mtorc1_param = sapply(model_num, function(num) {
-  bin_n = usefun::dec_to_bin(decimal_num = num, bits = n_bits)
-  substr(bin_n, mtorc1_index, mtorc1_index) %>% as.integer()
-})
+  lo_umap %>%
+    `colnames<-` (c("X", "Y")) %>%
+    tibble::as_tibble() %>%
+    tibble::add_column(node_lo = lo_mat[,node] %>% as.factor()) %>%
+    ggplot(aes(x = X, y = Y, colour = node_lo)) +
+    geom_point(shape = '.') +
+    scale_colour_brewer(palette = "Set1", labels = c("AND-NOT", "OR-NOT")) +
+    guides(colour = guide_legend(title = paste0(node, " LO"), label.theme = element_text(size = 12),
+      override.aes = list(shape = 19, size = 12))) +
+    labs(title = paste0("Parameterization Unsupervised Map (20 Neighbors)")) +
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5))
 
-print("Get CFLAR link operators")
-cflar_param = sapply(model_num, function(num) {
-  bin_n = usefun::dec_to_bin(decimal_num = num, bits = n_bits)
-  substr(bin_n, cflar_index, cflar_index) %>% as.integer()
-})
+  ggsave(filename = paste0("img/imp_nodes_param_ss_maps/unsup_", node, ".png"), dpi = "print", width = 7, height = 5)
+}
 
-# combine all data in a tibble
-data = lo_sumap %>%
-  as_tibble() %>%
-  mutate(ss_num = ss_num[model_samples_indexes_sup]) %>%
-  mutate(model_num = model_num) %>%
-  add_column(mapk14_param = mapk14_param, erkf_param = erkf_param,
-    mekf_param = mekf_param, pten_param = pten_param,
-    mtorc1_param = mtorc1_param, cflar_param = cflar_param)
+##############
+# Supervised #
+##############
 
-# plot UMAP maps
-ggplot(data, mapping = aes(x = X, y = Y)) +
-  geom_point(mapping = aes(color = factor(mapk14_param)), size = 0.01) +
-  theme_classic() +
-  scale_color_brewer(palette = "Set1", labels = c("AND-NOT", "OR-NOT")) +
-  guides(colour = guide_legend(title = "MAPK14 LO", label.theme = element_text(size = 12),
-    override.aes = list(size = 12))) #+
-# stat_ellipse(mapping = aes(fill = factor(ss_num)), alpha = 0.15, geom = "polygon", type = "norm") +
-#   guides(fill = guide_legend(title = "#Fixpoints", label.theme = element_text(size = 12), override.aes = list(size = 12)))
-ggsave(filename = "img/umap_supervised_MAPK14.png", dpi = "print", width = 7, height = 5)
+# euclidean metric, 14 neighbors, min_dist = 0.3
+for (node in imp_nodes) {
+  print(paste0("Node: ", node))
 
-ggplot(data) +
-  geom_point(aes(x = X, y = Y, color = factor(erkf_param)), size = 0.01) +
-  theme_classic() +
-  scale_colour_brewer(palette = "Set1", labels = c("AND-NOT", "OR-NOT")) +
-  guides(colour = guide_legend(title = "ERK_f LO", label.theme = element_text(size = 12),
-    override.aes = list(size = 12)))
-ggsave(filename = "img/umap_supervised_ERK_f.png", dpi = "print", width = 7, height = 5)
+  lo_sumap %>%
+    `colnames<-` (c("X", "Y")) %>%
+    tibble::as_tibble() %>%
+    tibble::add_column(node_lo = lo_mat[,node] %>% as.factor()) %>%
+    ggplot(aes(x = X, y = Y, colour = node_lo)) +
+    geom_point(shape = '.') +
+    scale_colour_brewer(palette = "Set1", labels = c("AND-NOT", "OR-NOT")) +
+    guides(colour = guide_legend(title = paste0(node, " LO"), label.theme = element_text(size = 12),
+      override.aes = list(shape = 19, size = 12))) +
+    labs(title = paste0("Parameterization Supervised Map (20 Neighbors)")) +
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5))
 
-ggplot(data) +
-  geom_point(aes(x = X, y = Y, color = factor(mekf_param)), size = 0.01) +
-  theme_classic() +
-  scale_colour_brewer(palette = "Set1", labels = c("AND-NOT", "OR-NOT")) +
-  guides(colour = guide_legend(title = "MEK_f LO", label.theme = element_text(size = 12),
-    override.aes = list(size = 12)))
-ggsave(filename = "img/umap_supervised_MEK_f.png", dpi = "print", width = 7, height = 5)
-
-ggplot(data) +
-  geom_point(aes(x = X, y = Y, color = factor(pten_param)), size = 0.01) +
-  theme_classic() +
-  scale_colour_brewer(palette = "Set1", labels = c("AND-NOT", "OR-NOT")) +
-  guides(colour = guide_legend(title = "PTEN LO", label.theme = element_text(size = 12),
-    override.aes = list(size = 12)))
-ggsave(filename = "img/umap_supervised_PTEN.png", dpi = "print", width = 7, height = 5)
-
-ggplot(data) +
-  geom_point(aes(x = X, y = Y, color = factor(mtorc1_param)), size = 0.01) +
-  theme_classic() +
-  scale_colour_brewer(palette = "Set1", labels = c("AND-NOT", "OR-NOT")) +
-  guides(colour = guide_legend(title = "mTORC1_c LO", label.theme = element_text(size = 12),
-    override.aes = list(size = 12)))
-ggsave(filename = "img/umap_supervised_mTORC1_c.png", dpi = "print", width = 7, height = 5)
-
-ggplot(data) +
-  geom_point(aes(x = X, y = Y, color = factor(cflar_param)), size = 0.01) +
-  theme_classic() +
-  scale_colour_brewer(palette = "Set1", labels = c("AND-NOT", "OR-NOT")) +
-  guides(colour = guide_legend(title = "CFLAR LO", label.theme = element_text(size = 12),
-    override.aes = list(size = 12)))
-ggsave(filename = "img/umap_supervised_CFLAR.png", dpi = "print", width = 7, height = 5)
+  ggsave(filename = paste0("img/imp_nodes_param_ss_maps/sup_", node, ".png"), dpi = "print", width = 7, height = 5)
+}
